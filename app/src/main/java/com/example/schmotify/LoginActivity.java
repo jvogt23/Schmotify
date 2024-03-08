4d8348d87fa33de785d5ddf94ed72c62eca20e96
Amanuel <amanu.ayalew@gmail.com>
2024-03-08 13:09:49 -0500

    Firebase config
    - firebase config stood up
    - initial user backend logic for login screens

---
+++ app/src/main/java/com/example/schmotify/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.schmotify;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+        private FirebaseAuth mAuth;
+
+        
+
+        }
