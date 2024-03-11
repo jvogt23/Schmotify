70858807b8498c91d39a610dd7415e3556a06c1b
Amanuel <amanu.ayalew@gmail.com>
2024-03-11 06:47:56 -0400

    Resource files for login UI
    - quick save of the sign in screen

--- app/src/main/java/com/example/schmotify/LoginActivity.java
+++ app/src/main/java/com/example/schmotify/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.schmotify;
 
-import android.app.Activity;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.schmotify.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-        private FirebaseAuth mAuth;
+        private ActivityMainBinding binding;
 
-        
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+
+                binding = ActivityMainBinding.inflate(getLayoutInflater());
+                setContentView(binding.getRoot());
 
         }
+
+}
