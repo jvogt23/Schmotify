cbd40649582d2f578632154b78e1d278c8f7d292
jvogt23 <91633361+jvogt23@users.noreply.github.com>
2024-03-13 16:15:49 -0400

    Added Spotify dependencies - Can now be integrated into login process

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,9 @@ android {
         versionCode = 1
         versionName = "1.0"
 
+        manifestPlaceholders["redirectSchemeName"] = "schmotify"
+        manifestPlaceholders["redirectHostName"] = "auth"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -40,6 +43,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation("com.spotify.android:auth:2.1.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
