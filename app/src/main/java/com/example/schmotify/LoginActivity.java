a96bc9b2413a887acec92d3b64cfd7d036134453
Amanuel <amanu.ayalew@gmail.com>
2024-03-26 15:33:54 -0400

    Clean up unused imports

--- app/src/main/java/com/example/schmotify/LoginActivity.java
+++ app/src/main/java/com/example/schmotify/LoginActivity.java
@@ -3,15 +3,10 @@ package com.example.schmotify;
 import android.os.Bundle;
 
 import com.example.schmotify.databinding.ActivityLoginBinding;
-import com.example.schmotify.ui.login.LoginFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -42,29 +37,5 @@ public class LoginActivity extends AppCompatActivity {
     public void onLogSwapClicked() {
         navController.navigate(R.id.action_navigation_register_to_navigation_login);
     }
-
-
-//        if (navHostFragment.getChildFragmentManager().getFragments().get(1).getView().findViewById(R.id.returnToLogin) != null) {
-//            TextView reg2Sign = (TextView) findViewById(R.id.returnToLogin);
-//            reg2Sign.setOnClickListener(new View.OnClickListener()
-//            {
-//                public void onClick(View view)
-//                {
-//                    navController.navigate(R.id.action_navigation_register_to_navigation_login);
-//                }
-//            });
-//        }
-//
-//
-//        if (navHostFragment.getChildFragmentManager().getFragments().get(2).getView().findViewById(R.id.retFromError) != null) {
-//            Button retFromError = (Button) findViewById(R.id.retFromError);
-//            retFromError.setOnClickListener(new View.OnClickListener()
-//            {
-//                public void onClick(View view)
-//                {
-//                    navController.navigate(R.id.action_navigation_error_to_navigation_register);
-//                }
-//            });
-//        }
 
 }
